36d0eeabe3e1cb8143d3e761ab755ac883b28dc3
Kit La Touche <kit.la.t@gmail.com>
2013-05-22 15:07:05 -0600

    Artwork first sprint.
    Lots of small things, conspiring to put artworks front and center,
    and reviews as dependent entities.

--- are_there_spiders/are_there_spiders/urls.py
+++ are_there_spiders/are_there_spiders/urls.py
@@ -12,7 +12,7 @@ from registration.forms import RegistrationFormUniqueEmail
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='base.html')),
-    url(r'^reviews/', include('phobias.urls', namespace='phobias')),
+    url(r'^media/', include('phobias.urls', namespace='phobias')),
     # Override the default registration view:
     url(r'^accounts/register/$', RegistrationView.as_view(
             form_class=RegistrationFormUniqueEmail,
--- are_there_spiders/phobias/urls.py
+++ are_there_spiders/phobias/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from .views import (
     CollectionView,
     CreateView,
-    InstanceView,
+    ArtworkInstanceView,
     EditView,
     DeleteView
 )
@@ -11,7 +11,7 @@ from .views import (
 urlpatterns = patterns('',
     url(r'^$', CollectionView.as_view(), name='collection'),
     url(r'^new/$', CreateView.as_view(), name='new'),
-    url(r'^(?P<pk>[^/]+)/$', InstanceView.as_view(), name='instance'),
+    url(r'^(?P<pk>[^/]+)/$', ArtworkInstanceView.as_view(), name='artwork_instance'),
     url(r'^(?P<pk>[^/]+)/edit/$', EditView.as_view(), name='edit'),
     url(r'^(?P<pk>[^/]+)/delete/$', DeleteView.as_view(), name='delete'),
 )
