a7f4f8a0ee0bb9b0b5090aaf6533cf65d91a4151
Kit La Touche <kit.la.t@gmail.com>
2013-06-07 16:11:43 -0600

    Added logo, favicon, etc.
    Also robots.txt, humans.txt, and URLs settings to find them in the
    static asset bucket.

--- are_there_spiders/are_there_spiders/urls.py
+++ are_there_spiders/are_there_spiders/urls.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -10,11 +12,14 @@ from registration.backends.default.views import RegistrationView
 from registration.forms import RegistrationFormUniqueEmail
 
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',  # No prefix
     url(r'^$', TemplateView.as_view(template_name='base.html')),
     url(r'^media/', include('phobias.urls', namespace='phobias')),
     # Override the default registration view:
-    url(r'^accounts/register/$', RegistrationView.as_view(
+    url(
+        r'^accounts/register/$',
+        RegistrationView.as_view(
             form_class=RegistrationFormUniqueEmail,
         ),
         name='registration_register'
@@ -24,4 +29,26 @@ urlpatterns = patterns('',
         include('registration.backends.default.urls')
     ),
     url(r'^admin/', include(admin.site.urls)),
+
+    url(
+        r'favicon\.ico$',
+        RedirectView.as_view(
+            url='%sfavicon.ico' % settings.STATIC_URL,
+            permanent=True
+        )
+    ),
+    url(
+        r'robots\.txt$',
+        RedirectView.as_view(
+            url='%srobots.txt' % settings.STATIC_URL,
+            permanent=True
+        )
+    ),
+    url(
+        r'humans\.txt$',
+        RedirectView.as_view(
+            url='%shumans.txt' % settings.STATIC_URL,
+            permanent=True
+        )
+    ),
 )
